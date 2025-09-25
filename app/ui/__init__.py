f93bc45fd0372c049dc9c492f33f598819b6608d
habibmubarak <habib_mubarak@hotmail.com>
2025-09-25 14:42:44 +0200

    Screens hinzugefügt

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,11 +1,29 @@
 from .home_screen import HomeScreen
-from .chat_screen import ChatScreen
+from .menu_screen import MenuScreen
+from .name_screen import NameScreen
+from .twoplayer_screen import TwoPlayerScreen
+from .select_character import SelectCharacter
 
+from kivy.core.text import LabelBase
 
-PRIMARY_PALETTE = "Blue"
-THEME_STYLE = "Light"
-BUTTON_BG = [61/256, 105/256, 153/256, 1]
+import json
+
+# Farben aus JSON laden
+settings = json.load(open("app/assets/settings/color.json", encoding="utf-8"))
+# Fonts aus JSON laden
+fonts = json.load(open("app/assets/settings/fonts.json", encoding="utf-8"))
+
+# Alle Fonts registrieren
+for font_name, font_path in fonts.items():
+    LabelBase.register(name=font_name, fn_regular=font_path) 
+
+PRIMARY_PALETTE = settings["PRIMARY_PALETTE"]
+THEME_STYLE = settings["THEME_STYLE"]
+BUTTON_BG = settings["LIGHT_BLUE"]
 
 def register_screens(screen_manager):
     screen_manager.add_widget(HomeScreen(name="home"))
-    screen_manager.add_widget(ChatScreen(name="chat"))
+    screen_manager.add_widget(NameScreen(name="name"))
+    screen_manager.add_widget(MenuScreen(name="menu"))
+    screen_manager.add_widget(TwoPlayerScreen(name="twoplayer"))
+    screen_manager.add_widget(SelectCharacter(name="select_character"))
