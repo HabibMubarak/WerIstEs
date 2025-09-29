16126ecb457115a6f647c7ae28c49d7e926df37c
habibmubarak <habib_mubarak@hotmail.com>
2025-09-29 13:13:49 +0200

    Permission für Android hinzugefügt

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
-
+from kivy import platform
 # Importiere ScreenManager-Setup aus deiner App
 from app.ui import register_screens, PRIMARY_PALETTE, THEME_STYLE # kümmert sich um das Hinzufügen der Screens
 import json
 
+if platform == "android":
+    from android.permissions import request_permissions, Permission, check_permission  # pylint: disable=import-error # type: ignore
+    request_permissions([Permission.READ_EXTERNAL_STORAGE,
+                        Permission.WRITE_EXTERNAL_STORAGE,
+                        Permission.CAMERA,
+                        ])
 
 class AnimeWerIstDasApp(MDApp):
     def build(self):
@@ -15,6 +21,18 @@ class AnimeWerIstDasApp(MDApp):
         sm = ScreenManager()
         register_screens(sm)  # Screens werden hier hinzugefügt
         return sm
+    
+    def on_enter(self):
+        # Berechtigungen für Android anfragen
+        if platform == "android":
+            print("Berechtigungen werden angefragt...")
+            request_permissions([
+                Permission.READ_EXTERNAL_STORAGE,
+                Permission.WRITE_EXTERNAL_STORAGE,
+                Permission.CAMERA,
+                Permission.FLASHLIGHT,
+            ])
+            print("Berechtigungen wurden angefragt.")
 
 if __name__ == "__main__":
     AnimeWerIstDasApp().run()
