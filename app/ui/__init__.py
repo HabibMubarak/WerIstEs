df55786291f4ef9c60539cd783d0a5be3d795e98
habibmubarak <habib_mubarak@hotmail.com>
2025-10-05 20:38:16 +0200

    Package

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -7,6 +7,8 @@ from .select_character import SelectCharacter
 from .setting_screen import SettingScreen
 from .anime_selection_screen import AnimeSelectionScreen
 from .game_field_screen import GameFieldScreen
+from .ready_screen import ReadyScreen
+
 from kivy.core.text import LabelBase
 
 import json
@@ -25,12 +27,14 @@ THEME_STYLE = settings["THEME_STYLE"]
 BUTTON_BG = settings["LIGHT_BLUE"]
 
 def register_screens(screen_manager):
+
     screen_manager.add_widget(HomeScreen(name="home"))
     screen_manager.add_widget(NameScreen(name="name"))
     screen_manager.add_widget(AnimeSelectionScreen(name="anime_selection"))
     screen_manager.add_widget(MenuScreen(name="menu"))
     screen_manager.add_widget(WaitingRoomScreen(name="waiting_room"))
     screen_manager.add_widget(TwoPlayerScreen(name="twoplayer"))
+    screen_manager.add_widget(ReadyScreen(name="ready"))
     screen_manager.add_widget(SelectCharacter(name="select_character"))
     screen_manager.add_widget(GameFieldScreen(name="game_field"))
     screen_manager.add_widget(SettingScreen(name="setting"))
